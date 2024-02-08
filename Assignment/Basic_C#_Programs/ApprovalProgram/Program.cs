5b819f42bfa66ba722051e281d01aa214149098f
ACReturns <DSXtreme2012@gmail.com>
2024-02-08 01:43:26 -0800

    Approval Application assignment
    - Setup approval app to accept user input
    - Compare the responses to ensure that if all were true then validate that the user was eligible

---
+++ Assignment/Basic_C#_Programs/ApprovalProgram/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApprovalProgram
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            string userAge = Console.ReadLine();
+            Console.WriteLine("Users Age: " + userAge);
+            Console.ReadLine();
+
+            Console.WriteLine("Have you ever had a DUI? (true or false)");
+            string userResponse = Console.ReadLine();
+            bool duiResult;
+            Console.WriteLine("User Response: " + userResponse);
+            Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string userTickets = Console.ReadLine();
+            Console.WriteLine("User has had " + userTickets + " speeding tickets");
+            Console.ReadLine();
+
+            bool userQuailified = Convert.ToInt64(userAge) > 15 && Boolean.TryParse(userResponse, out duiResult) == true && 
+                Convert.ToInt64(userTickets) < 3;
+
+            Console.WriteLine("User quailified for car insurance? " + userQuailified);
+            Console.ReadLine();
+        }
+    }
+}
