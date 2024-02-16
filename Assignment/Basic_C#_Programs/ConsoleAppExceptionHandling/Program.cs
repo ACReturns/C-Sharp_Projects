4dae867782fadc0e342835d8bf9b91bb14d8c51d
ACReturns <DSXtreme2012@gmail.com>
2024-02-16 15:34:32 -0800

    Exception Handler Assignment
    - Setup exception handler assignment to divide 5 numbers by what the user input

---
+++ Assignment/Basic_C#_Programs/ConsoleAppExceptionHandling/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppExceptionHandling
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                List<int> savedNumbers = new List<int>() { 3, 15, 7, 9, 10 };
+                Console.WriteLine("Please enter a number to divide 5 numbers by.");
+                int userNumber = Convert.ToInt32((string)Console.ReadLine());
+
+                foreach (int number in savedNumbers)
+                {
+                    int numberResult = number / userNumber;
+                    Console.WriteLine(number + " divided by " + userNumber + " equals " + numberResult);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please enter a whole number");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("You attempted to divide by 0");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Try/ Catch process has completed.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
