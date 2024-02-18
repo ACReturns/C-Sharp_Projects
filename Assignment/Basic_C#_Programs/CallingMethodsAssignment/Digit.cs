64ba87b22e54a758d0bb881697002f8b176fc025
ACReturns <DSXtreme2012@gmail.com>
2024-02-18 12:49:13 -0800

    Calling Method Assignment
    - Setup Methods in a class to be called from the main program file to do math

---
+++ Assignment/Basic_C#_Programs/CallingMethodsAssignment/Digit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+	public class Digit
+	{
+		public int Multiply(int n) 
+		{
+			return n * 5;
+		}
+
+        public int Add(int n)
+        {
+            return n + 20;
+        }
+
+        public int Divide(int n)
+        {
+            return n / 2;
+        }
+    }
+}
---
+++ Assignment/Basic_C#_Programs/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to do calculations with");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            Digit digit = new Digit();
+
+            Console.WriteLine(digit.Multiply(userNumber));
+            Console.WriteLine(digit.Divide(userNumber));
+            Console.WriteLine(digit.Add(userNumber));
+
+            Console.ReadLine();
+        }
+    }
+}
