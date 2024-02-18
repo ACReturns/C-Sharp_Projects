2337959126c33b08852cb0cf4efa05a97fcb6c67
ACReturns <DSXtreme2012@gmail.com>
2024-02-17 23:29:57 -0800

    TwentyOne Card game
    - Setup classes to generate card suit/ faces as well as the deck with all 52 cards
    - Setup a method to shuffle the cards on boot

---
+++ Assignment/Basic_C#_Programs/TwentyOne/Deck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+	public class Deck
+	{
+		public Deck() 
+		{
+            Cards = new List<Card>();
+            string[] Faces = { "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", 
+				"Nine", "Ten", "Jack", "Queen", "King", "Ace" };
+			string[] Suits = { "Spades", "Hearts", "Diamonds", "Clubs" };
+
+			foreach (string Face in Faces) 
+			{
+				foreach (string Suit in Suits) 
+				{
+                    Card card = new Card();
+                    card.Face = Face;
+                    card.Suit = Suit;
+                    Cards.Add(card);
+                }
+			}
+		}
+		public List<Card> Cards {  get; set; }
+	}
+}
