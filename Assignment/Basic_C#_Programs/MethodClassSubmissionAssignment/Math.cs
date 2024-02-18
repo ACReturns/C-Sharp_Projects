f04f4526d1c58329f45e76ab46025b221bbcc5d2
ACReturns <DSXtreme2012@gmail.com>
2024-02-18 15:07:41 -0800

    Method Class Submission
    - Setup a void method that did a simple math function followed by a console.writeline
    - Called the function both normal & by specifying the parameters by name

---
+++ Assignment/Basic_C#_Programs/MethodClassSubmissionAssignment/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmissionAssignment
+{
+    public class Math
+    {
+        public void DoMath(int value1, int value2)
+        {
+            int result = value1 * 2;
+            Console.WriteLine(value2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment/Basic_C#_Programs/MethodClassSubmissionAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmissionAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            math.DoMath(value1: 20, value2: 77);
+        }
+    }
+}
