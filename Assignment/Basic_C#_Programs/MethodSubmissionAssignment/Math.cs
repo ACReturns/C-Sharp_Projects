64a0de4a4d1211c230335ea2c4f99d57ca2ee839
ACReturns <DSXtreme2012@gmail.com>
2024-02-18 14:52:59 -0800

    Method Submission Assignment
    - Created a class with a method that took an optional input
    - Handled avoiding converting a string to int if the value was 0

---
+++ Assignment/Basic_C#_Programs/MethodSubmissionAssignment/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    public class Math
+    {
+        public int DoubleTrouble(int value1, int value2 = 5)
+        {
+            return value1 * value2;
+        }
+    }
+}
---
+++ Assignment/Basic_C#_Programs/MethodSubmissionAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            int secondNumber = 0;
+            Console.WriteLine("Please enter two numbers one at a time. The second one you can leave blank:");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            string secondInput = Console.ReadLine();
+
+            // Can't convert an empty string to int so confirming it has a value before utilizing the conversion
+            if (secondInput.Length != 0)
+            {
+                secondNumber = Convert.ToInt32(secondInput);
+            }
+
+            Console.WriteLine(math.DoubleTrouble(firstNumber));
+            Console.ReadLine();
+        }
+    }
+}
