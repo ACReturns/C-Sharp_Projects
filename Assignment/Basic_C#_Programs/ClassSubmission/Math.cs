e4419d4adb8b624745d644d05e75937c6adf3ab2
ACReturns <DSXtreme2012@gmail.com>
2024-02-18 16:39:06 -0800

    Class Submission
    - Created a class with void & int methods
    - Created a static method
    - Did math with the input from the user as well as the result in following math problems

---
+++ Assignment/Basic_C#_Programs/ClassSubmission/Math.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    public class Math
+    {
+        public void Divide(out int result, int value)
+        {
+            result = value / 2;
+            Console.WriteLine(value + " / " + 2 + " = " + result);
+        }
+
+        public int Divide(int value1, int value2)
+        {
+            Console.WriteLine(value1 + " / " + value2 + " = ");
+            return value1 / value2;
+        }
+    }
+}
---
+++ Assignment/Basic_C#_Programs/ClassSubmission/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            int result = 0;
+
+            Console.Write("Please enter a number: ");
+            int userInput = Convert.ToInt32((string)Console.ReadLine());
+
+            math.Divide(out result, userInput);
+            Console.ReadLine();
+
+            result = math.Divide(userInput, result);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+            var decimalResult = Divide(userInput, result);
+            Console.WriteLine(userInput + " / " + result + " = " + decimalResult);
+            Console.ReadLine();
+        }
+
+        public static decimal Divide(decimal value1, decimal value2)
+        {
+            return value1 / value2; ;
+        }
+    }
+}
