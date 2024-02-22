72eee6762732a2f97985791f5b0e20ee21ab4eb6
ACReturns <DSXtreme2012@gmail.com>
2024-02-21 22:56:11 -0800

    Days of the week assignment
    - Setup enum for days of the week
    - Setup a enum parse to compare the input text from the user to the days of the week in the enum collection

---
+++ Assignment/Basic_C#_Programs/DaysOfTheWeek/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfTheWeek
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter the current day of the week:");
+
+			try
+			{
+				Console.WriteLine("The day you entered was " + Enum.Parse(typeof(DaysOfTheWeek), Console.ReadLine()));
+			}
+			catch (Exception ex) 
+			{
+				Console.WriteLine("Please enter an actual day of the week");
+			}
+			finally 
+			{
+				Console.ReadLine();
+			}
+		}
+
+		public enum DaysOfTheWeek
+		{
+			Monday,
+			Tuesday,
+			Wednesday,
+			Thursday,
+			Friday,
+			Saturday,
+			Sunday
+		}
+	}
+}
