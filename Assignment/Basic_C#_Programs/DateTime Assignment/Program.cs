972111bf55c849714517b7375b0058c4361856fb
ACReturns <DSXtreme2012@gmail.com>
2024-02-25 21:41:56 -0800

    DateTime Assignment
    - Setup Datetime output to get the current date/ time on the users computer
    - Adds whatever number the user inputs in hours to then output the future time

---
+++ Assignment/Basic_C#_Programs/DateTime Assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DateTime_Assignment
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(DateTime.Now);
+			Console.WriteLine("Please enter a number: ");
+			int userResponse = Convert.ToInt32(Console.ReadLine());
+			
+			// Adds the number input as hours 
+			DateTime futureTime = DateTime.Now.AddHours(userResponse);
+			Console.WriteLine("In " + userResponse + " hours the time will be " + futureTime);
+			Console.ReadLine();
+		}
+	}
+}
